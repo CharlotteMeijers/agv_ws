6b1e45e82dd73ac1f99fb94675189c245ab84698
Charlotte <c.s.meijers@student.utwente.nl>
2025-04-09 16:21:57 +0200

    adjusted localisation file for zinger

--- agv_pkg/launch/zinger_loc.launch.py
+++ agv_pkg/launch/zinger_loc.launch.py
@@ -1,90 +1,68 @@
-
 import os
-
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
-                            RegisterEventHandler)
-from launch.conditions import IfCondition
-from launch.events import matches_action
-from launch.substitutions import (AndSubstitution, LaunchConfiguration,
-                                  NotSubstitution)
-from launch_ros.actions import LifecycleNode
-from launch_ros.event_handlers import OnStateTransition
-from launch_ros.events.lifecycle import ChangeState
-from lifecycle_msgs.msg import Transition
 
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+
+from launch_ros.actions import Node, PushRosNamespace
+
+from nav2_common.launch import RewrittenYaml
+
+
+ARGUMENTS = [
+    DeclareLaunchArgument('use_sim_time', default_value='false',
+                          choices=['true', 'false'],
+                          description='Use sim time'),
+]
 
 def generate_launch_description():
+    package_name='agv_pkg' 
+
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
-    slam_params_file = LaunchConfiguration('slam_params_file')
-    autostart = LaunchConfiguration('autostart')
+    ld = LaunchDescription(ARGUMENTS)
 
-    declare_use_sim_time_argument = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='true',
-        description='Use simulation/Gazebo clock')
-    declare_slam_params_file_cmd = DeclareLaunchArgument(
-        'slam_params_file',
-        default_value=os.path.join(get_package_share_directory("slam_toolbox"),
-                                   'config', 'localisation_params_online_async.yaml'),
-        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
-    declare_autostart_cmd = DeclareLaunchArgument(
-        'autostart', default_value='true',
-        description='Automatically startup the slamtoolbox. '
-                    'Ignored when use_lifecycle_manager is true.')
-    declare_use_lifecycle_manager = DeclareLaunchArgument(
-        'use_lifecycle_manager', default_value='false',
-        description='Enable bond connection during node activation')
+    map_location = os.path.join(get_package_share_directory(package_name),'config','home_map.yaml')
 
-    start_localization_slam_toolbox_node = LifecycleNode(
-        parameters=[
-          slam_params_file,
-          {
-            'use_lifecycle_manager': use_lifecycle_manager,
-            'use_sim_time': use_sim_time
-          }
-        ],
-        package='slam_toolbox',
-        executable='localization_slam_toolbox_node',
-        name='slam_toolbox',
+    map_server = Node(
+        package='nav2_map_server',
+        executable='map_server',
         output='screen',
-        namespace=''
+        parameters=[
+            {'yaml_filename' : map_location,
+            'use_sim_time': use_sim_time}
+        ],
     )
+    ld.add_action(map_server)
 
-    configure_event = EmitEvent(
-        event=ChangeState(
-            lifecycle_node_matcher=matches_action(start_localization_slam_toolbox_node),
-            transition_id=Transition.TRANSITION_CONFIGURE
-        ),
-        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    amcl_server = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        output='screen',
+        parameters=[
+            {'use_sim_time': use_sim_time}
+        ],
     )
+    ld.add_action(amcl_server)
 
-    activate_event = RegisterEventHandler(
-        OnStateTransition(
-            target_lifecycle_node=start_localization_slam_toolbox_node,
-            start_state="configuring",
-            goal_state="inactive",
-            entities=[
-                LogInfo(msg="[LifecycleLaunch] Slamtoolbox node is activating."),
-                EmitEvent(event=ChangeState(
-                    lifecycle_node_matcher=matches_action(start_localization_slam_toolbox_node),
-                    transition_id=Transition.TRANSITION_ACTIVATE
-                ))
-            ]
-        ),
-        condition=IfCondition(AndSubstitution(autostart, NotSubstitution(use_lifecycle_manager)))
+    # Activate map_server and amcl node
+    activate_nodes = TimerAction(
+        period=1.0,
+        actions= [
+            Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_localization',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['map_server'
+                                        ,'amcl'
+                                        ]}])
+        ]
     )
-
-    ld = LaunchDescription()
-
-    ld.add_action(declare_use_sim_time_argument)
-    ld.add_action(declare_slam_params_file_cmd)
-    ld.add_action(declare_autostart_cmd)
-    ld.add_action(declare_use_lifecycle_manager)
-    ld.add_action(start_localization_slam_toolbox_node)
-    ld.add_action(configure_event)
-    ld.add_action(activate_event)
+    ld.add_action(activate_nodes)
 
     return ld
