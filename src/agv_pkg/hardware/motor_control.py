85bda4863f555cc7686faa9609c8b8fd191d45c9
Charlotte Meijers <charlotte.meijers@student.utwente.nl>
2025-05-08 14:23:59 +0200

    steering motor encoder test

--- src/agv_pkg/hardware/motor_control.py
+++ src/agv_pkg/hardware/motor_control.py
@@ -5,11 +5,14 @@ from rclpy.lifecycle import LifecycleNode, LifecycleState, TransitionCallbackRet
 from std_msgs.msg import Float64MultiArray
 import can
 import struct
+import subprocess
 
 duty_cycle_mode = 0x2050080
+position_mode = 0x2050C80
 
-drive_ids = [11, 21, 31, 41]
-steer_ids = [12, 22, 32, 42]
+drive_ids = [1, 21, 31, 41]
+steer_ids = [2, 22, 32, 42]
+
 
 class MotorControlNode(LifecycleNode):
   def __init__(self):
@@ -19,6 +22,7 @@ class MotorControlNode(LifecycleNode):
     self.drive_sub = None
     self.steer_sub = None
     self.active = False
+    self.encoder_angles = {i: 0.0 for i in steer_ids}
 
   def on_configure(self, state: LifecycleState):
     self.get_logger().info("IN on_configure")
@@ -29,6 +33,12 @@ class MotorControlNode(LifecycleNode):
     self.steer_sub = self.create_subscription(Float64MultiArray, '/drive_module_steer_angle_controller/commands', self.steer_callback, 10)
     self.active = False
 
+    try:
+      subprocess.run(["sudo", "ip", "link", "set", "can0", "up", "type", "can", "bitrate", "1000000"], check=True)
+      self.get_logger().info("CAN interface can0 is up.")
+    except subprocess.CalledProcessError as e:
+      self.get_logger().error(f"Failed to bring up CAN interface: {e}")
+
     return TransitionCallbackReturn.SUCCESS
 
   def on_cleanup(self, state: LifecycleState):
@@ -36,6 +46,9 @@ class MotorControlNode(LifecycleNode):
     self.bus.shutdown()
     self.destroy_subscription(self.drive_sub)
     self.destroy_subscription(self.steer_sub)
+    subprocess.run(["sudo", "ip", "link", "set", "can0", "down"], check=True)
+    self.get_logger().info("CAN interface can0 is down.")
+
     self.active = False
     return TransitionCallbackReturn.SUCCESS
 
@@ -70,21 +83,60 @@ class MotorControlNode(LifecycleNode):
       self.get_logger().info(f'Received steer commands: {msg.data}')
       for i, value in enumerate(msg.data):
         if i < len(steer_ids):
-          self.send_control_frame(steer_ids[i], duty_cycle_mode, float(value))
+          target_angle = float(value)
+          motor_id = steer_ids[i]
+          self.get_logger().info(f"Setting steering motor {motor_id} to desired angle: {target_angle}")
+
+          while not self.has_reached_position(motor_id, target_angle):
+            self.send_control_frame(motor_id, position_mode, target_angle)
+            self.read_steering_motor_position()
+            rclpy.spin_once(self, timeout_sec=0.01)  # Little break
+            self.get_logger().info(f"Steering motor {motor_id} reached position {target_angle}")
+
+
+  # def steer_callback(self, msg: Float64MultiArray):
+  #   if self.active:
+  #     self.get_logger().info(f'Received steer commands: {msg.data}')
+  #     for i, value in enumerate(msg.data):
+  #       if i < len(steer_ids):
+  #         target_angle = float(value)
+  #         self.get_logger().info(f"Setting steering motor {steer_ids[i]} to desired position: {target_angle}")
+
+  #           while not self.has_reached_position(steer_ids[i], target_angle):
+  #             self.send_control_frame(steer_ids[i], duty_cycle_mode, 0.1)
+  #             rclpy.spin_once(self) 
+  #             self.get_logger().info(f"Steering motor {steer_ids[i]} reached desired position: {target_angle}")
 
   def send_control_frame(self, device_id: int, control_mode: int, setpoint: float):
     can_id = control_mode + device_id 
-    data = struct.pack('<f', setpoint) + bytes([0]*4) # Convert the float data into bytes based on little-endian (lowest byte first)
+    data = struct.pack('<f', setpoint) + bytes([0]*4) # Convert the float data into bytes based on little-endian (lowest byte first)  
     msg = can.Message(
       arbitration_id = can_id, data = data, is_extended_id = True
     )
-
     try:
       self.bus.send(msg)
       self.get_logger().info("Motor control frame sent")
     except can.CanError:
         self.get_logger().info("Motor control frame could not be send")
 
+  def read_steering_motor_position(self):
+    try:
+      msg = self.bus.recv()  
+      for i, value in enumerate(steer_ids):
+        if msg.arbitration_id == value:
+          self.encoder_angles[i] = sturct.unpack('f', bytes(msg.data[:4]))[0]
+          self.get_logger().info(f"Steering motor {i} position: {self.encoder_angles[i]}")
+        except can.CanError as e:
+          self.get_logger().error(f"CAN Error: {e}")
+
+  def has_reached_position(self, motor_id: int, target_angle: float) -> bool:
+    current_position = self.encoder_angles.get(motor_id, 0.0)
+    tolerance = 0.1  
+        
+    if abs(current_position - target_angle) <= tolerance:
+      return True
+    else:
+      return False
 
 def main(args=None):
   rclpy.init(args=args)
