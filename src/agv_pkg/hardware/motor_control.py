138ce8e5da14fede3c7b891e7ea2d15a95e856d4
Charlotte Meijers <charlotte.meijers@student.utwente.nl>
2025-05-13 14:36:15 +0200

    multi thread for the callback

--- src/agv_pkg/hardware/motor_control.py
+++ src/agv_pkg/hardware/motor_control.py
@@ -3,6 +3,7 @@
 import rclpy
 from rclpy.lifecycle import LifecycleNode, LifecycleState, TransitionCallbackReturn
 from std_msgs.msg import Float64MultiArray
+from rclpy.executors import MultiThreadedExecutor
 import can
 import struct
 import subprocess
@@ -14,7 +15,6 @@ drive_ids = [1, 21, 31, 41]
 steer_ids = [2, 22, 32, 42]
 encoder_reset = [0.0, 0.0, 0.0, 0.0]
 
-
 class MotorControlNode(LifecycleNode):
   def __init__(self):
     super().__init__("motor_control_node")
@@ -24,6 +24,8 @@ class MotorControlNode(LifecycleNode):
     self.steer_sub = None
     self.active = False
     self.encoder_angles = encoder_reset
+    self.steering_target = [None] * len(steer_ids) #Variable to store the target position for the steering motors
+    self.create_timer(0.1, self.update_steering_targets) #Update the steering position with 10Hz
 
   def on_configure(self, state: LifecycleState):
     self.get_logger().info("IN on_configure")
@@ -86,11 +88,13 @@ class MotorControlNode(LifecycleNode):
       for i, value in enumerate(msg.data):
         if i < len(steer_ids):
           self.get_logger().info(f"Setting steering motor {steer_ids[i]} to desired position: {float(value)}")
+          self.steering_target[i] = float(value)
+#          self.read_steering_messages(steer_ids[i])
 
-          while not self.has_reached_position(self.read_steering_messages(steer_ids[i]), float(value)):
-            self.send_control_frame(steer_ids[i], duty_cycle_mode, 0.1)
-            rclpy.spin_once(self, timeout_sec=0.1)
-          self.get_logger().info(f"Steering motor {i} reached desired position: {float(value)}")
+#          while not self.has_reached_position(self.read_steering_messages(steer_ids[i]), float(value)):
+#            self.send_control_frame(steer_ids[i], duty_cycle_mode, 0.1)
+#            rclpy.spin_once(self, timeout_sec=0.1)
+#          self.get_logger().info(f"Steering motor {i} reached desired position: {float(value)}")
 
   def send_control_frame(self, device_id: int, control_mode: int, setpoint: float):
     can_id = control_mode + device_id 
@@ -105,40 +109,44 @@ class MotorControlNode(LifecycleNode):
         self.get_logger().info("Motor control frame could not be send")
 
   def read_steering_messages(self, motor_id: int):
-    while True: # Make sure it keeps reading till it finds a message that fits the requirements
-      msg = self.bus.recv()
-      if msg is None:
-        continue
+#    while True: # Make sure it keeps reading till it finds a message that fits the requirements
+    msg = self.bus.recv(timeout=0.01)
+#      self.get_logger().info(f"Received message: {msg}")
+    if msg is None:
+      return None
         
-      can_id = msg.arbitration_id
+    can_id = msg.arbitration_id
+#      self.get_logger().info(f"Received can_id: {can_id}")
 
-      device_id = can_id & 0x3F
-      api_index = (can_id & 0x3C0) >> 6
-      api_class = (can_id & 0x1C00) >> 10
-      manufacturer = (can_id & 0xFF0000) >> 16
-      device_type = (can_id & 0x1F000000) >> 24
+    device_id = can_id & 0x3F
+    api_index = (can_id & 0x3C0) >> 6
+    api_class = (can_id & 0x1C00) >> 10
+    manufacturer = (can_id & 0xFF0000) >> 16
+    device_type = (can_id & 0x1F000000) >> 24
 
-      self.get_logger().info(f"[CAN ID] Device ID: {device_id}, API Class: {api_class}, API Index: {api_index}, Manufacturer: {manufacturer}, Device Type: {device_type}")
+    self.get_logger().info(f"{can_id}, Motor ID: {motor_id}, Device ID: {device_id}, API Class: {api_class}, API Index: {api_index}, Manufacturer: {manufacturer}, Device Type: {device_type}")
       
-      if device_id == motor_id and api_class == 6 and api_index == 2 :
-        steering_ratio = 9424 / 203
+#      if device_id == motor_id and api_class == 6 and api_index == 0 :
+#        self.get_logger().info(f"Received message: {msg}")
+#        steering_ratio = 9424 / 203
 
         #Convert bytes to float based on little-endian (lowest byte first)
-        raw_rpm = struct.unpack('<f', msg.data[0:4])[0]
-        raw_position = struct.unpack('<f', msg.data[4:8])[0] 
+#        raw_rpm = struct.unpack('<f', msg.data[0:4])[0]
+#        raw_position = struct.unpack('<f', msg.data[4:8])[0] 
 
-        rpm = (raw_rpm / steering_ratio) 
-        turning_angle = rpm / 60 * 360 # In degrees per seconds
-        accumulated_angle = raw_position/steering_ratio *360 #in degrees
+#        rpm = (raw_rpm / steering_ratio) 
+#        turning_angle = rpm / 60 * 360 # In degrees per seconds
+#        accumulated_angle = raw_position/steering_ratio *360 #in degrees
 
-        angle_degrees = accumulated_angle % 360 # Remove full rounds
-        if angle_degrees < 0:
-            angle_degrees += 360
-        norm_angle = angle_degrees / 360 # The normalised angle 
+#        angle_degrees = accumulated_angle % 360 # Remove full rounds
+#        self.get_logger().info(f"raw_rpm: {raw_rpm}, raw_position: {raw_position}, rpm: {rpm}, turning_angle: {turning_angle}, accumulated_angle: {accumulated_angle}, angle_degrees: {angle_degrees}")
+#        if angle_degrees < 0:
+#            angle_degrees += 360
+#        norm_angle = angle_degrees / 360 # The normalised angle 
+#        self.get_logger().info(f"normalised angle: {norm_angle}")
 
-
-        self.get_logger().info(f"Raw position: {raw_position}, rpm: {rpm}, turning angle per second {turning_angle}, accumalted angle {accumulated_angle}, normalised angle {norm_angle}")
-        return norm_angle
+#        self.get_logger().info(f"Raw position: {raw_position}, rpm: {rpm}, turning angle per second {turning_angle}, accumalted angle {accumulated_angle}, normalised angle {norm_angle}")
+#        return norm_angle
 
 
   # def read_steering_motor_position(self):
@@ -153,17 +161,40 @@ class MotorControlNode(LifecycleNode):
 
   def has_reached_position(self, current_position: float, target_angle: float) -> bool:
     tolerance = 0.1  
-        
-    if abs(current_position - target_angle) <= tolerance:
-      return True
+#    self.get_logger().info(f"current position: {current_position}, target_angle: {target_angle}")
+    if current_position is not None:
+      if abs(current_position - target_angle) <= tolerance:
+        return True
+      else:
+        return False
     else:
-      return False
+        return False #False such that it will just turn without listening to feedback
+
+  def update_steering_targets(self):
+    if not self.active:
+      return
+    for i, target_position in enumerate(self.steering_target):
+      if target_position is not None:
+         current_position = self.read_steering_messages(steer_ids[i])
+         if not self.has_reached_position(current_position, target_position):
+            self.send_control_frame(steer_ids[i], duty_cycle_mode, 0.1)
+         else:
+            self.send_control_frame(steer_ids[i], duty_cycle_mode, 0.0)
+            self.get_logger().info(f"Steering motor {steer_ids[i]} reached target position {target_position}")
+            self.steering_target[i] = None
 
 def main(args=None):
   rclpy.init(args=args)
   lifecycle_node = MotorControlNode()
-  rclpy.spin(lifecycle_node)
-  rclpy.shutdown()
+
+  executor = MultiThreadedExecutor()
+  executor.add_node(lifecycle_node)
+
+  try:
+     executor.spin()
+  finally:
+     lifecycle_node.destroy_node()
+     rclpy.shutdown()
 
 if __name__ == "__main__":
   main()
